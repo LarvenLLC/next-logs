0a1f0f2dcc0c3d981ebfb61c9bd080178d7dd17d
BossBele <adambeleko@gmail.com>
2022-07-08 23:51:50 +0300

    init create-react-library@3.1.1

---
+++ example/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'next-logs'
+import 'next-logs/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
