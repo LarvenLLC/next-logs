ebfbafbe58b1c8a2fb3e451356902b16bc18c57f
BossBele <adambeleko@gmail.com>
2022-07-09 01:08:22 +0300

    correct json error

--- src/api.js
+++ src/api.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
         res.status(405).end(`Method ${method} Not Allowed`)
     }
     // send result
-    return res.status(200).send('Success')
+    return res.status(200).json({ message: 'Success' })
   } catch (error) {
     return res.status(500).json(error)
   }
