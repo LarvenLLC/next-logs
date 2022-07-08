af3d51e457dd522821b138710be09bd7dfa9f9a4
BossBele <adambeleko@gmail.com>
2022-07-08 22:19:45 +0300

    init

---
+++ logger.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+
+const logFiles = {
+  console: 'logs/console.log',
+  info: 'logs/info.log',
+  debug: 'logs/debug.log',
+  warn: 'logs/warn.log',
+  error: 'logs/error.log',
+};
+
+const Logger = {
+  console: new console.Console(fs.createWriteStream(logFiles.console, {
+    flags: 'a', // 'a' means appending (old data will be preserved)
+  })),
+  info: new console.Console(fs.createWriteStream(logFiles.info, {
+    flags: 'a', // 'a' means appending (old data will be preserved)
+  })),
+  debug: new console.Console(fs.createWriteStream(logFiles.debug, {
+    flags: 'a', // 'a' means appending (old data will be preserved)
+  })),
+  warn: new console.Console(fs.createWriteStream(logFiles.warn, {
+    flags: 'a', // 'a' means appending (old data will be preserved)
+  })),
+  error: new console.Console(fs.createWriteStream(logFiles.error, {
+    flags: 'a', // 'a' means appending (old data will be preserved)
+  })),
+};
+
+// logs in .log file for simplicity
+function log(message = '', attributes = {}, type = 'info') {
+  const date = new Date();
+  const timestamp = `${date.toISOString().split('T')[0]} ${date.toLocaleTimeString()}`;
+  const msg = `${timestamp} LOGGER.${type.toUpperCase()}: ${message}`;
+  Logger.console.log(msg, attributes);
+  Logger[type].log(msg, attributes);
+}
+
+const logger = {
+  log,
+  info: (message = '', attributes = {}) => log(message, attributes, 'info'),
+  debug: (message = '', attributes = {}) => log(message, attributes, 'debug'),
+  warn: (message = '', attributes = {}) => log(message, attributes, 'warn'),
+  error: (message = '', attributes = {}) => log(message, attributes, 'error'),
+};
+export default logger;
