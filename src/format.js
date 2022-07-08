ad53bedfcf958ce9bb808ca3b7b7adacf7b810cd
BossBele <adambeleko@gmail.com>
2022-07-09 01:25:41 +0300

    return msg

--- src/format.js
+++ src/format.js
@@ -1,5 +1,9 @@
 export default function format(message, type) {
-  const date = new Date();
-  const timestamp = `${date.toISOString().split('T')[0]} ${date.toLocaleTimeString()}`;
-  const msg = `${timestamp} LOGGER.${type.toUpperCase()}: ${message}`;
+  const date = new Date()
+  const timestamp = `${
+    date.toISOString().split('T')[0]
+  } ${date.toLocaleTimeString()}`
+  const msg = `${timestamp} LOGGER.${type.toUpperCase()}: ${message}`
+
+  return msg
 }
