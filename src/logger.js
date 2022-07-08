2775552e04d38515c9c8e94233560adc06df1550
BossBele <adambeleko@gmail.com>
2022-07-09 00:57:06 +0300

    error handling

--- src/logger.js
+++ src/logger.js
@@ -9,39 +9,44 @@ const logFiles = {
   error: 'logs/error.log'
 }
 
-const Logger = {
-  console: new console.Console(
-    fs.createWriteStream(logFiles.console, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    })
-  ),
-  info: new console.Console(
-    fs.createWriteStream(logFiles.info, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    })
-  ),
-  debug: new console.Console(
-    fs.createWriteStream(logFiles.debug, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    })
-  ),
-  warn: new console.Console(
-    fs.createWriteStream(logFiles.warn, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    })
-  ),
-  error: new console.Console(
-    fs.createWriteStream(logFiles.error, {
-      flags: 'a' // 'a' means appending (old data will be preserved)
-    })
-  )
+const Logger = () => {
+  if (!fs || !('createWriteStream' in fs)) {
+    return {}
+  }
+  return {
+    console: new console.Console(
+      fs.createWriteStream(logFiles.console, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    ),
+    info: new console.Console(
+      fs.createWriteStream(logFiles.info, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    ),
+    debug: new console.Console(
+      fs.createWriteStream(logFiles.debug, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    ),
+    warn: new console.Console(
+      fs.createWriteStream(logFiles.warn, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    ),
+    error: new console.Console(
+      fs.createWriteStream(logFiles.error, {
+        flags: 'a' // 'a' means appending (old data will be preserved)
+      })
+    )
+  }
 }
 
 // logs in .log file for simplicity
 function log(message = '', attributes = {}, type = 'info') {
   const msg = format(message, type)
-  Logger.console.log(msg, attributes)
-  Logger[type].log(msg, attributes)
+  Logger().console.log(msg, attributes)
+  Logger()[type].log(msg, attributes)
 }
 
 const logger = {
