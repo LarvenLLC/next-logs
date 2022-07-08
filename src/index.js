715fb1e65fd58d79c44dc1d8bbac159b7d118abe
BossBele <adambeleko@gmail.com>
2022-07-09 00:33:27 +0300

    next logs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
-import React from 'react'
-import styles from './styles.module.css'
+import logger from './logger'
+import NextLogs from './api'
+import log from './client'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+module.exports = log
+module.exports.logger = logger
+module.exports.NextLogs = NextLogs
+// export { logger, NextLogs }
+// export default log
