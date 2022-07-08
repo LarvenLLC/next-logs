1fc47563bbea136aa76f35c105dd2c835a07f8a6
BossBele <adambeleko@gmail.com>
2022-07-09 00:33:18 +0300

    next logs

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import logger from './logger'
+
+export default async function handler(req, res) {
+  const {
+    method,
+    body: { message = '', attributes = {} },
+    query: { log = 'info' }
+  } = req
+
+  try {
+    switch (method) {
+      case 'POST':
+        // log
+        logger.log(message, attributes, log)
+        break
+      default:
+        res.setHeader('Allow', ['POST'])
+        res.status(405).end(`Method ${method} Not Allowed`)
+    }
+    // send result
+    return res.status(200).send('Success')
+  } catch (error) {
+    return res.status(500).json(error)
+  }
+}
