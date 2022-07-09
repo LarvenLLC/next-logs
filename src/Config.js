3a5b03d68670527042c00bced274db1b97393765
BossBele <adambeleko@gmail.com>
2022-07-10 01:28:32 +0300

    api

---
+++ src/Config.js
@@ -0,0 +1,34 @@
+class Config {
+  constructor(settings = {}) {
+    const { dir = '' } = settings
+
+    this.dir = dir
+    this.logFiles = {
+      console: `${dir}/console.log`,
+      info: `${dir}/info.log`,
+      debug: `${dir}/debug.log`,
+      warn: `${dir}/warn.log`,
+      error: `${dir}/error.log`
+    }
+  }
+
+  setDir(dir) {
+    this.dir = dir
+    this.updateLogFiles()
+  }
+
+  updateLogFiles() {
+    const dir = this.dir
+    this.logFiles = {
+      console: `${dir}/console.log`,
+      info: `${dir}/info.log`,
+      debug: `${dir}/debug.log`,
+      warn: `${dir}/warn.log`,
+      error: `${dir}/error.log`
+    }
+  }
+}
+
+const config = new Config()
+
+export default config
