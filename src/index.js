eadf48d88207465b07c4c635c53876db1e7e68d1
BossBele <adambeleko@gmail.com>
2022-08-06 13:28:18 +0300

    exported logger from API init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
-import Logger from './Logger'
-import NextLogs, { config } from './api'
+import NextLogs, { loggerFunction } from './api'
 import log from './client'
 
 module.exports = log
-module.exports.logger = new Logger(config.dir, config.logFiles).logger
+module.exports.logger = loggerFunction
 module.exports.NextLogs = NextLogs
-// export { logger, NextLogs, log }
