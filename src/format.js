87e442080952bd6db58af4f2f064b2aaefe39c01
BossBele <adambeleko@gmail.com>
2022-08-11 18:39:54 +0300

    update apiupdate api

--- src/format.js
+++
@@ -1,9 +0,0 @@
-export default function format(message, type) {
-  const date = new Date()
-  const timestamp = `[${
-    date.toISOString().split('T')[0]
-  } ${date.toLocaleTimeString()}]`
-  const msg = `${timestamp} LOGGER.${type.toUpperCase()}: ${message}`
-
-  return msg
-}
